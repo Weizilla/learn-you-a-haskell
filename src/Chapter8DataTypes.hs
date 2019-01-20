2ce98dc9235c40454c0a2bc734764d74f5bffbf0
Wei Yang <wei@weizilla.com>
2019-01-20 08:53:59 -0600

    Chapter 8 stuff

--- src/Chapter8DataTypes.hs
+++ src/Chapter8DataTypes.hs
@@ -83,3 +83,32 @@ treeElem x (Node n treeL treeR)
     | x < n = treeElem x treeL
     | x > n = treeElem x treeR
     | x == n = True
+
+data TrafficLight = Red | Yellow | Green
+
+instance Eq TrafficLight where
+    Red == Red = True
+    Green == Green = True
+    Yellow == Yellow = True
+    _ == _ = False
+
+instance Show TrafficLight where
+    show Red = "Red light"
+    show Yellow = "Yellow light"
+    show Green = "Green light"
+
+class YesNo a where
+    yesno :: a -> Bool
+
+instance YesNo Int where
+    yesno 0 = False
+    yesno _ = True
+
+instance YesNo [a] where
+    yesno [] = False
+    yesno _ = True
+
+instance YesNo (Maybe a) where
+    yesno (Just _) = True
+    yesno Nothing = False
+
