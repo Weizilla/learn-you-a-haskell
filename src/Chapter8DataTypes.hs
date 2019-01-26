a89cee00ea7eeb39078d88844d24123034ca5b68
Wei Yang <wei@weizilla.com>
2019-01-26 07:10:52 -0600

    Formatting

--- src/Chapter8DataTypes.hs
+++ src/Chapter8DataTypes.hs
@@ -2,17 +2,9 @@ module Chapter8DataTypes where
 
 import qualified Data.Map as Map
 
-data Shape
-    = Circle Point
-             Float
-    | Rectangle Point
-                Point
-    deriving (Show)
+data Shape = Circle Point Float | Rectangle Point Point deriving (Show)
 
-data Point =
-    Point Float
-          Float
-    deriving (Show)
+data Point = Point Float Float deriving (Show)
 
 surface :: Shape -> Float
 surface (Circle _ r) = pi * r ^ 2
@@ -26,11 +18,7 @@ data Person = Person
 
 p = Person { firstName = "Joe", lastName = "Smith", age = 10 }
 
-data Vector a =
-    Vector a
-           a
-           a
-    deriving (Show)
+data Vector a = Vector a a a deriving (Show)
 
 vplus :: (Num t) => Vector t -> Vector t -> Vector t
 vplus (Vector x y z) (Vector x2 y2 z2) = Vector (x + x2) (y + y2) (z + z2)
