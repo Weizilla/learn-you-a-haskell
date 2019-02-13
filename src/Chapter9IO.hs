530a66df58eccbb14b4bbf060fdd3742e16e7f19
Wei Yang <wei@weizilla.com>
2019-02-13 07:05:19 -0600

    Chapter 9

--- src/Chapter9IO.hs
+++ src/Chapter9IO.hs
@@ -1,6 +1,7 @@
 module Chapter9IO where
 
 import Data.Char
+import Control.Monad
 
 printStuff :: IO ()
 printStuff = do
@@ -20,3 +21,23 @@ reverseAll = do
 
 reverseWords :: String -> String
 reverseWords = unwords . map reverse . words
+
+doColors :: IO ()
+doColors = do
+  colors <- forM [1, 2, 3, 4] (\a -> do
+    putStrLn $ "What color for " ++ show a ++ "?"
+    getLine)
+  putStrLn "These are your colors:"
+  mapM_ putStrLn colors
+
+shortLines :: IO ()
+shortLines = do
+  contents <- getContents
+  putStr (shortLinesOnly contents)
+
+shortLinesOnly :: String -> String
+shortLinesOnly input =
+  let allLines = lines input
+      short = filter (\l -> length l < 10) allLines
+      result = unlines short
+  in result
